50a0842c59833e40ef7349e22c71f96bd48e4338
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-13 18:16:56 +0100

    chore: added constants to extension entry point

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
-const EXTENSION_NAME = "codesharp";
-const EXTENSION_ID = "codesharp";
-const CREATE_FILE_COMMAND = `${EXTENSION_NAME}.createFile`;
-const CREATE_CLASS_COMMAND = `${EXTENSION_NAME}.createClass`;
-const CREATE_INTERFACE_COMMAND = `${EXTENSION_NAME}.createInterface`;
-const CREATE_ENUM_COMMAND = `${EXTENSION_NAME}.createEnum`;
-const CREATE_RECORD_COMMAD = `${EXTENSION_NAME}.createRecord`;
-const CREATE_STRUCT_COMMAND = `${EXTENSION_NAME}.createStruct`;
+export const EXTENSION_NAME = "codesharp";
+export const EXTENSION_ID = "codesharp";
+export const CREATE_FILE_COMMAND = `${EXTENSION_NAME}.createFile`;
+export const CREATE_CLASS_COMMAND = `${EXTENSION_NAME}.createClass`;
+export const CREATE_INTERFACE_COMMAND = `${EXTENSION_NAME}.createInterface`;
+export const CREATE_ENUM_COMMAND = `${EXTENSION_NAME}.createEnum`;
+export const CREATE_RECORD_COMMAD = `${EXTENSION_NAME}.createRecord`;
+export const CREATE_STRUCT_COMMAND = `${EXTENSION_NAME}.createStruct`;
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,14 @@
 import * as vscode from "vscode";
+import {
+  CREATE_CLASS_COMMAND,
+  CREATE_ENUM_COMMAND,
+  CREATE_FILE_COMMAND,
+  CREATE_INTERFACE_COMMAND,
+  CREATE_RECORD_COMMAD,
+  CREATE_STRUCT_COMMAND,
+} from "./constants";
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand(""))
 }
 
 export function deactivate() {}
